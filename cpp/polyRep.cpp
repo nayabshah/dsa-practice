e8621c9b20e64c637e81ae5d43a10eac8bf74ec3
Ariba <77058329+nayabshah@users.noreply.github.com>
2026-09-15 19:05:03 +0000

    Add initial implementation of Polynomial and Term classes in C++

---
+++ cpp/polyRep.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Term{
+public:
+    int coeff;
+    int exp;
+};
+class Polynomial{
+private:
+    int n;
+    Term *t;
+};
+
+int main(){
+    return 0;
+}
