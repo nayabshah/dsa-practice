fe02c569a31ea5ed05fb2a8fc5ee99d605496366
Ariba <77058329+nayabshah@users.noreply.github.com>
2026-09-16 13:18:02 +0000

    Add Polynomial class implementation with term input and evaluation

--- cpp/polyRep.cpp
+++ cpp/polyRep.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 class Term{
 public:
@@ -6,11 +8,26 @@ public:
     int exp;
 };
 class Polynomial{
-private:
+public:
     int n;
     Term *t;
 };
 
+
 int main(){
+    Polynomial p;
+    cout<< "No of non-zero terms: ";
+    cin>>p.n;
+    p.t = new Term[p.n];
+    for(int i=0;i<p.n;i++){
+        cout<<"Enter coeff and exp of term "<<i+1<<": ";
+        cin>>p.t[i].coeff>>p.t[i].exp;
+    }
+    cout<<"Enter the value of x: ";
+    double x;
+    cin>>x;
+    for(int i=0;i<p.n;i++){
+        double sum = p.t[i].coeff* std::pow(x, p.t[i].exp);
+    };
     return 0;
 }
